b5cfaef3c37db27df1b0f5c40e6eb2c73cf6322f
Confidence Okoghenun <confidenceboi@gmail.com>
2020-08-06 20:19:40 +0100

    refactor: Conditionally loads dotenv

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
-require('dotenv').config()
+import dotenv from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 export const PORT = process.env.PORT
 export const DB_URL = process.env.DB_URL
