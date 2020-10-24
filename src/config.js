9c484deeb46f756a77adb8a506fead3025775ec7
cokoghenun <confidenceboi@gmail.com>
2020-10-24 02:45:18 +0100

    feat: Updates redis connection

--- src/config.js
+++ src/config.js
@@ -8,7 +8,6 @@ export const USER_DB = process.env.USER_DB
 export const MUSIC_DB = process.env.MUSIC_DB
 export const POSTMARK_TOKEN = process.env.POSTMARK_TOKEN
 export const REDIS_CONFIG = {
-  host: process.env.REDIS_URL,
+  host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD
 }
