138d0ef0fbc1e3dc830ce5709311b2c78b439abc
cokoghenun <confidenceboi@gmail.com>
2020-10-24 03:19:24 +0100

    feat: Updates redis connection

--- src/auth/validate/limiter.js
+++ src/auth/validate/limiter.js
@@ -1,7 +1,7 @@
 import redis from 'ioredis'
 import redisStore from 'rate-limit-redis'
 import rateLimit from 'express-rate-limit'
-import { REDIS_CONFIG } from '../../config'
+import { REDIS_URL } from '../../config'
 
 const max = 1000
 
@@ -10,7 +10,7 @@ const limiter = rateLimit({
   delayMs: 0,
   store: new redisStore({
     expiry: 24 * 60 * 60,
-    client: new redis(REDIS_CONFIG),
+    client: new redis(REDIS_URL),
   }),
   message: {
     error: `You have exhausted your free ${max} request quota, please try again in 24 hours or consider upgrading`,
--- src/config.js
+++ src/config.js
@@ -7,7 +7,4 @@ export const GTAG_ID = process.env.GTAG_ID
 export const USER_DB = process.env.USER_DB
 export const MUSIC_DB = process.env.MUSIC_DB
 export const POSTMARK_TOKEN = process.env.POSTMARK_TOKEN
-export const REDIS_CONFIG = {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-}
+export const REDIS_URL = process.env.REDIS_URL
