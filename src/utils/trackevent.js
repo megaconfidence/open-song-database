5389f5a7f5fa32264dca856a97a83c14e6fdbcf9
cokoghenun <confidenceboi@gmail.com>
2021-01-03 09:30:44 +0100

    fix: Fixed tracker

--- src/utils/trackevent.js
+++ src/utils/trackevent.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'
 import { GTAG_ID } from '../config'
 
-const trackEvent = (category, action, label, value) => {
+const trackEvent = (category, action, label, value=1) => {
   return fetch(
     `http://www.google-analytics.com/collect?v=1&cid=555&t=event&tid=${GTAG_ID}&dp=%2F${category}&ec=${category}&ea=${action}&el=${label}&ev=${value}`,
     {
       headers: {
         'user-agent':
-          'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0',
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
       },
     }
   )
